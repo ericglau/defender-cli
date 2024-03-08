df9bbdf1254fe04141f0c7af1275c283e7a0c4e2
Eric Lau <ericglau@outlook.com>
2024-03-08 17:27:04 -0500

    Check for duplicate chainIds, support DEFENDER_NETWORK env var

--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const { getNetwork } = require('../dist/internal/utils');
 
-test('returns defender network definition', async t => {
+test('getNetwork finds network', async t => {
   const fakeNetworkClient = {
     listForkedNetworks: () => {
       return []
@@ -14,7 +14,7 @@ test('returns defender network definition', async t => {
   t.is(network, 'goerli');
 });
 
-test('fails if chain id is not accepted', async t => {
+test('getNetwork cannot find network', async t => {
   const fakeNetworkClient = {
     listForkedNetworks: () => {
       return [];
@@ -24,10 +24,10 @@ test('fails if chain id is not accepted', async t => {
     },
   };
 
-  await t.throwsAsync(() => getNetwork(0x123456, fakeNetworkClient), { message: /Network \d+ is not supported/ });
+  await t.throwsAsync(() => getNetwork(0x123456, fakeNetworkClient), { message: /The current network with chainId \d+ is not supported/, });
 });
 
-test('forked network', async t => {
+test('getNetworks finds forked network', async t => {
   const fakeNetworkClient = {
     listForkedNetworks: () => {
       return [
@@ -50,7 +50,7 @@ test('forked network', async t => {
   t.is(network, 'my-forked-network');
 });
 
-test('private network', async t => {
+test('getNetwork finds private network', async t => {
   const fakeNetworkClient = {
     listForkedNetworks: () => {
       return [];
@@ -67,4 +67,97 @@ test('private network', async t => {
 
   const network = await getNetwork(0x123456, fakeNetworkClient);
   t.is(network, 'my-private-network');
+});
+
+test('getNetwork finds multiple networks', async t => {
+  const fakeNetworkClient = {
+    listForkedNetworks: () => {
+      return [];
+    },
+    listPrivateNetworks: () => {
+      return [
+        {
+          chainId: 0x123456,
+          name: 'first-forked-network',
+        },
+        {
+          chainId: 0x123456,
+          name: 'second-forked-network',
+        },
+      ];
+    },
+  };
+
+  await t.throwsAsync(() => getNetwork(0x123456, fakeNetworkClient), {
+    message:
+      /Detected multiple networks with the same chainId \d+ on OpenZeppelin Defender: first-forked-network, second-forked-network/,
+  });
+});
+
+test('getNetworks finds one network, does not match specified network', async t => {
+  const fakeNetworkClient = {
+    listForkedNetworks: () => {
+      return [
+        {
+          chainId: 0x123456,
+          name: 'my-forked-network',
+        },
+      ];
+    },
+    listPrivateNetworks: () => {
+      return [];
+    },
+  };
+
+  await t.throwsAsync(() => getNetwork(0x123456, fakeNetworkClient, 'specified-network'), {
+    message: /Detected network my-forked-network does not match specified network: specified-network/,
+  });
+});
+
+test('getNetworks finds multiple networks, does not match specified network', async t => {
+  const fakeNetworkClient = {
+    listForkedNetworks: () => {
+      return [
+        {
+          chainId: 0x123456,
+          name: 'my-forked-network',
+        },
+        {
+          chainId: 0x123456,
+          name: 'my-forked-network-2',
+        },
+      ];
+    },
+    listPrivateNetworks: () => {
+      return [];
+    },
+  };
+
+  await t.throwsAsync(() => getNetwork(0x123456, fakeNetworkClient, 'specified-network'), {
+    message:
+      /Specified network specified-network does not match any of the detected networks for chainId 1193046: my-forked-network, my-forked-network-2/,
+  });
+});
+
+test('getNetworks finds multiple networks, includes specified network', async t => {
+  const fakeNetworkClient = {
+    listForkedNetworks: () => {
+      return [
+        {
+          chainId: 0x123456,
+          name: 'my-forked-network',
+        },
+        {
+          chainId: 0x123456,
+          name: 'specified-network',
+        },
+      ];
+    },
+    listPrivateNetworks: () => {
+      return [];
+    },
+  };
+
+  const network = await getNetwork(0x123456, fakeNetworkClient, 'specified-network');
+  t.is(network, 'specified-network');
 });
